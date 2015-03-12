49e28f13bce3de82d6d581c2e9bf2d075f78d9e7
Alex Sims <alexmk92@live.co.uk>
2015-03-12 12:53:48 +0000

    Added the .h file for the Camera class.

--- ASCamera.h
+++ ASCamera.h
@@ -25,4 +25,41 @@
 ******************************************************************
 */
 
+class ASCamera
+{
+public:
+	// Constructors and Destructors
+	ASCamera();
+	ASCamera(const ASCamera&);
+	~ASCamera();
+
+	// Public methods
+	void SetRotation(float, float, float);
+	void SetPosition(float, float, float);
+
+	// Getters to retrieve the rotation and position Matrices
+	D3DXVECTOR3 GetRotation();	
+	D3DXVECTOR3 GetPosition();
+
+	// Render the view
+	void Render();
+
+	// Write the View matrix to a given D3DXMATRIX object
+	void GetViewMatrix(D3DXMATRIX&);
+
+private:
+	// X, Y, Z Positions of the projection matrix
+	float m_posX;
+	float m_posY;
+	float m_posZ;
+
+	// X, Y, Z Positions of the rotation matrix
+	float m_rotX;
+	float m_rotY;
+	float m_rozZ;
+
+	//  Matrix to describe what we are looking at (the eye of the camera)
+	D3DXMATRIX m_cameraView;
+};
+
 #endif
